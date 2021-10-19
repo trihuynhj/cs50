9b7fafb845806651e8ebbca589d3052ab3698317
Tri <tri.huynh.j@gmail.com>
2021-10-19 22:11:52 +0900

    pset9 - part 28

--- pset9/application.py
+++ pset9/application.py
@@ -151,3 +151,16 @@ def login():
         # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 400)
+        
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
