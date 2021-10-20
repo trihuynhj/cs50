2af59141d0876d3b3a7af403659b0ce2b2184700
Tri <tri.huynh.j@gmail.com>
2021-10-20 21:25:32 +0900

    pset9 - part 30

--- pset9/application.py
+++ pset9/application.py
@@ -170,3 +170,12 @@ def login():
         return render_template("login.html")
 
 
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
