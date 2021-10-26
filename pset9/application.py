eb02305874bd303d93713041afec2bdac5271bae
Tri <tri.huynh.j@gmail.com>
2021-10-26 23:49:06 +0900

    pset9 - Finance

--- pset9/application.py
+++ pset9/application.py
@@ -13,10 +13,8 @@ from helpers import apology, login_required, lookup, usd
 # Configure application
 app = Flask(__name__)
 
-
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
 
 # Ensure responses aren't cached
 @app.after_request
@@ -26,10 +24,8 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
 # Custom filter
 app.jinja_env.filters["usd"] = usd
-
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -37,10 +33,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
-
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
