48a1542cc8316cf97a7b466692a0a823994dd450
Tri <tri.huynh.j@gmail.com>
2021-10-26 23:52:58 +0900

    finalproject - tests - test.py - part 1

---
+++ finalproject/tests/test.py
@@ -0,0 +1,5 @@
+# EMAIL CONFIGURATION AND SENDING TEST (PYTHON'S SMTPLIB, SSL, MIME LIBRARIES)
+
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
