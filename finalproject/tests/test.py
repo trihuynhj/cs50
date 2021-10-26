525b04b6565261a2ed697162f796ce827c672f46
Tri <tri.huynh.j@gmail.com>
2021-10-26 23:53:31 +0900

    finalproject - tests - test.py - part 2

--- finalproject/tests/test.py
+++ finalproject/tests/test.py
@@ -3,3 +3,13 @@
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+
+sender_email = "picprank.cs50@gmail.com"
+sender_password = "Thisiscs50#"
+
+receiver_email = input("Enter the receiver's email address: ")
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "From PicPrank.CS50 with Laughters"
+message["from"] = sender_email
+message["to"] = receiver_email
