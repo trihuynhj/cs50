33e0c4b86a1b3c69d19100f93957d23a2566cc72
Tri <tri.huynh.j@gmail.com>
2021-10-29 23:30:52 +0900

    finalproject - tests - test.py

--- finalproject/tests/test.py
+++ finalproject/tests/test.py
@@ -24,3 +24,14 @@ html = """\
   </body>
 </html>
 """
+
+# Turn these into plain/html MIMEText objects
+content = MIMEText(html, "html")
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(content)
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context()) as server:
+    server.login(sender_email, sender_password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
