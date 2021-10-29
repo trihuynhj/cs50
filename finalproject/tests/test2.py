d2f08e33d85e5ee0b0234536af176c1b6382f7d6
Tri <tri.huynh.j@gmail.com>
2021-10-29 23:33:50 +0900

    finalproject - tests - test2.py - part 2

--- finalproject/tests/test2.py
+++ finalproject/tests/test2.py
@@ -2,3 +2,8 @@
 
 import requests
 from serpapi import GoogleSearch
+
+#url = "https://serpapi.com/account?api_key=00608aa6196298194939714d009218033442b305aaa9d243099ea7862716b085"
+#response = requests.get(url).json()
+
+#print(response)
