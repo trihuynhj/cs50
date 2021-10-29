04e52961a3e3de863748cd3a95e395358d5cc80d
Tri <tri.huynh.j@gmail.com>
2021-10-29 23:32:00 +0900

    finalproject - tests - test1.py

--- finalproject/tests/test1.py
+++ finalproject/tests/test1.py
@@ -6,3 +6,12 @@ string = input("Provide a string: ")
 
 result1 = re.findall("[0-9]", string)
 result2 = re.findall("[!@#$%^&*()_+]", string)
+
+if not result1 and not result2:
+    print("No numbers and/or special symbols")
+elif not result1 and result2:
+    print("No numbers, but has special symbols")
+elif result1 and not result2:
+    print("No symbols, but has numbers")
+else:
+    print(result1, result2)
