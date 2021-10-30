7f31633fcf4ee05d8fdf0d78ab173cfbbc46dffb
Tri <tri.huynh.j@gmail.com>
2021-10-30 16:58:52 +0900

    finalproject - tests - test2.py - part 3

--- finalproject/tests/test2.py
+++ finalproject/tests/test2.py
@@ -7,3 +7,16 @@ from serpapi import GoogleSearch
 #response = requests.get(url).json()
 
 #print(response)
+
+key = input("Provide a keyword to search: ")
+API_KEY = "00608aa6196298194939714d009218033442b305aaa9d243099ea7862716b085"
+
+params = {
+    "engine": "google",
+    "q": key,
+    "hl": "en",
+    "gl": "us",
+    "google_domain": "google.com",
+    "tbm": "isch",                  # Image Search
+    "api_key": API_KEY
+}
