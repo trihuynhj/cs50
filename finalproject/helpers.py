abb1c19314e887ad56ec9a17676eb54a2650f271
Tri <tri.huynh.j@gmail.com>
2021-11-09 21:38:56 +0900

    finalproject- helpers.py - part 4

--- finalproject/helpers.py
+++ finalproject/helpers.py
@@ -7,3 +7,5 @@ import smtplib, ssl
 from flask import redirect, render_template, request, session
 from functools import wraps
 from serpapi import GoogleSearch
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
