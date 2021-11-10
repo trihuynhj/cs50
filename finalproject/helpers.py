67e1c972c169dcb76b906c5856b40ec0047300ab
Tri <tri.huynh.j@gmail.com>
2021-11-10 21:27:33 +0900

    finalproject - helpers.py - part 7

--- finalproject/helpers.py
+++ finalproject/helpers.py
@@ -15,6 +15,10 @@ def error(message, code=400):
     def escape(s):
         """
         Escape special characters.
-        
+
         https://github.com/jacebrowning/memegen#special-characters
         """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
