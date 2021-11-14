23b6485909f8ba32ec400a329f9f6dc80e8d1443
Tri <tri.huynh.j@gmail.com>
2021-11-14 17:30:26 +0900

    finalproject - helpers.py - part 30

--- finalproject/helpers.py
+++ finalproject/helpers.py
@@ -142,3 +142,7 @@ def send_email(user, receiver_email, response, time):
 
     # Turn these into plain/html MIMEText objects
     content = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(content)
