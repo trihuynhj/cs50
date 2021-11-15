02c772af780b7e53b58b46eafa73a9f214599a49
Tri <tri.huynh.j@gmail.com>
2021-11-15 21:14:42 +0900

    finalproject - application.py - part 7

--- finalproject/application.py
+++ finalproject/application.py
@@ -18,3 +18,7 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    
