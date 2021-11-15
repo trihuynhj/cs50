bd5b98d33d53af15c10cf73a727abe813fd61e01
Tri <tri.huynh.j@gmail.com>
2021-11-15 21:13:18 +0900

    finalproject - application.py - part 4

--- finalproject/application.py
+++ finalproject/application.py
@@ -8,3 +8,4 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
