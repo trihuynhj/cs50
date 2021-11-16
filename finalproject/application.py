1b4fd8017e1b27ad79f0639ebfdf209e2c6838ca
Tri <tri.huynh.j@gmail.com>
2021-11-16 22:40:00 +0900

    finalproject - application.py - part 9

--- finalproject/application.py
+++ finalproject/application.py
@@ -26,3 +26,9 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
