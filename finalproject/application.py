3d7f10d3fea69ad499c67d12f2f20e5c38758ff2
Tri <tri.huynh.j@gmail.com>
2021-11-16 22:41:07 +0900

    finalproject - application.py - part 10

--- finalproject/application.py
+++ finalproject/application.py
@@ -32,3 +32,5 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///database.db")
